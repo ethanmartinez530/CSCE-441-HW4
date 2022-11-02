efbd76e79233dee5bef17df748048e5cc20b09c6
ethanmartinez530 <90010138+ethanmartinez530@users.noreply.github.com>
2022-11-02 14:22:03 -0500

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,167 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+#include "Program.h"
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+#define NUM_LIGHTS 2
+#define NUM_MATERIALS 3
+
+GLFWwindow *window;
+
+Program program;
+std::vector<float> posBuff;
+std::vector<float> norBuff;
+std::vector<float> texBuff;
+
+glm::vec3 eye(0.0f, 0.0f, 4.0f);
+
+struct materialStruct {
+	glm::vec3 ka, kd, ks;
+	float s;
+} materials[NUM_MATERIALS];
+
+struct lightStruct {
+	glm::vec3 position;
+	glm::vec3 color;
+} lights[NUM_LIGHTS];
+
+
+void Display()
+{		
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+
+	glm::mat4 projectionMatrix = glm::perspective(glm::radians(45.0f), float(width) / float(height), 0.1f, 100.0f);
+	glm::mat4 viewMatrix = glm::lookAt(eye, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	glm::mat4 modelMatrix(1.0f);
+	modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.2f, -1.0f, 0.0f)) * glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	program.Bind();
+	program.SendUniformData(modelMatrix, "model");
+	program.SendUniformData(viewMatrix, "view");
+	program.SendUniformData(projectionMatrix, "projection");
+	glDrawArrays(GL_TRIANGLES, 0, posBuff.size() / 3);
+	program.Unbind();
+
+}
+
+// Keyboard character callback function
+void CharacterCallback(GLFWwindow* lWindow, unsigned int key)
+{
+	switch (key) 
+	{
+	case 'q':
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+		break;
+	default:
+		break;
+	}
+}
+
+
+void FrameBufferSizeCallback(GLFWwindow* lWindow, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void LoadModel(char* name)
+{
+	// Taken from Shinjiro Sueda with slight modification
+	std::string meshName(name);
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	std::string errStr;
+	bool rc = tinyobj::LoadObj(&attrib, &shapes, &materials, &errStr, meshName.c_str());
+	if (!rc) {
+		std::cerr << errStr << std::endl;
+	}
+	else {
+		// Some OBJ files have different indices for vertex positions, normals,
+		// and texture coordinates. For example, a cube corner vertex may have
+		// three different normals. Here, we are going to duplicate all such
+		// vertices.
+		// Loop over shapes
+		for (size_t s = 0; s < shapes.size(); s++) {
+			// Loop over faces (polygons)
+			size_t index_offset = 0;
+			for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+				size_t fv = shapes[s].mesh.num_face_vertices[f];
+				// Loop over vertices in the face.
+				for (size_t v = 0; v < fv; v++) {
+					// access to vertex
+					tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+					posBuff.push_back(attrib.vertices[3 * idx.vertex_index + 0]);
+					posBuff.push_back(attrib.vertices[3 * idx.vertex_index + 1]);
+					posBuff.push_back(attrib.vertices[3 * idx.vertex_index + 2]);
+					if (!attrib.normals.empty()) {
+						norBuff.push_back(attrib.normals[3 * idx.normal_index + 0]);
+						norBuff.push_back(attrib.normals[3 * idx.normal_index + 1]);
+						norBuff.push_back(attrib.normals[3 * idx.normal_index + 2]);
+					}
+					if (!attrib.texcoords.empty()) {
+						texBuff.push_back(attrib.texcoords[2 * idx.texcoord_index + 0]);
+						texBuff.push_back(attrib.texcoords[2 * idx.texcoord_index + 1]);
+					}
+				}
+				index_offset += fv;
+				// per-face material (IGNORE)
+				shapes[s].mesh.material_ids[f];
+			}
+		}
+	}
+}
+
+void Init()
+{
+	glfwInit();
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Assignment4 - <Your Name>", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glewExperimental = GL_TRUE;
+	glewInit();
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	glfwSetCharCallback(window, CharacterCallback);
+	glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glEnable(GL_DEPTH_TEST);
+
+	LoadModel("../obj/bunny.obj");
+	
+	program.SetShadersFileName("../shaders/shader.vert", "../shaders/shader.frag");
+	program.Init();
+	program.SendAttributeData(posBuff, "vPositionModel");
+	program.SendAttributeData(norBuff, "vNormalModel");
+}
+
+
+int main()
+{	
+	Init();
+	while ( glfwWindowShouldClose(window) == 0) 
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		Display();
+		glFlush();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
